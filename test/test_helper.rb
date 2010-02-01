f9b29f94d7527cc585d25737592977a34054c6ea
Eric Allam <rubymaverick@gmail.com>
2010-02-01 09:19:43 -0500

    extend MiniTest::Unit::TestCase with shoulda capibilities, now tests will run

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,19 @@
 $:.reject! { |e| e.include? 'TextMate' }
 
 require 'rubygems'
-require 'minitest/unit'
 require 'shoulda'
+require 'minitest/unit'
 require 'mocha'
 
 require File.dirname(__FILE__) + '/../lib/garb'
 
 class MiniTest::Unit::TestCase
+  
+  include Shoulda::InstanceMethods
+  extend Shoulda::ClassMethods
+  include Shoulda::Assertions
+  extend Shoulda::Macros
+  include Shoulda::Helpers
 
   def read_fixture(filename)
     File.read(File.dirname(__FILE__) + "/fixtures/#{filename}")
